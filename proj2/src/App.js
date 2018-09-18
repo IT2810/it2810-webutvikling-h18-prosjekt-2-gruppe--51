1a17f647223094b2ac377a7d0357e9e2079d706f
Rikard Eriksen <rikard.s.eriksen@gmail.com>
2018-09-18 14:14:00 +0200

    media randomizer function
    currently only implemented for the images,
    but the function is easily reusable for the other media types as well

--- proj2/src/App.js
+++ proj2/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
 
   constructor(props) {
     super(props)
-    this.state = {text: {author: "placeholder", text:"bob"}};
+    this.state = { text: { author: "placeholder", text: "bob" }, image: "lol image here pls" };
     this.generateMedia = this.generateMedia.bind(this);
   }
 
@@ -18,37 +18,49 @@ class App extends Component {
     const print = Object.values(results);
     const prant = Object.keys(results)
     alert("you have selected: " + prant + " : " + print);
-    // idea: save the response of a get into a variable?
-    // eg. this.setState ({image: res.dataFromAsvgFetchWhenActivated})
     axios.get("assets/text/facts/1.json")
-    .then(res => {
-      console.log(res);
-      this.setState({text: res.data});
-  })
+      .then(res => {
+        console.log(res);
+        this.setState({ text: res.data });
+      });
+
+    // todo: feed this URL to the tab components, and let them make an axios get when they are active
+    let imageUrl = this.randomMedia({ folder: "images", format: ".svg" }, [results["image"]])
+    console.log(imageUrl)
+    axios.get(imageUrl).then(res => { this.setState({ image: res.data }) });
+  };
+
+  randomMedia(type, categories) {
+    let i = Math.round(Math.random() * (categories.length - 1))
+    let i2 = Math.round(Math.random() * 3 + 1)
+    return "assets/" + type["folder"] + "/" + categories[i] + "/" + i2 + type["format"]
   }
 
-  // todo: make static version with some basic grid css
-  // todo: hook up selector panel to a randomizer function
   // todo: create tab element that can feed its contents to the media area
-    // idea: have a top parent being the media area itself, where the state resides?
+  // idea: have a top parent being the media area itself, where the state resides?
 
   render() {
+    const image = this.state["image"];
     return (
       <div className="App">
         <div className="MediaArea">
-          <img src="assets/images/night/1.svg" alt=""></img>
+          <div className="GalleryImage" dangerouslySetInnerHTML={{ __html: image }} />
           <div className="TextField"><h1>{this.state.text["author"]}</h1>{this.state.text["text"]}</div>
         </div>
         <div className="TabRow">
           <div className="t1">Tab1</div>
         </div>
         <div className="ToolPanel">
-          <SelectorPanel groups={[{groupName: "image", 
-          legend: "Choose an image category", 
-          options: ["art", "abstract", "animals"]},
-          {groupName: "text",
-          legend: "Choose a text type", 
-          options: ["haiku", "jokes", "facts"]}]} onSubmit={this.generateMedia} buttonText="Create my image!"/>
+          <SelectorPanel groups={[{
+            groupName: "image",
+            legend: "Choose an image category",
+            options: ["abstract", "night", "icons"]
+          },
+          {
+            groupName: "text",
+            legend: "Choose a text type",
+            options: ["haiku", "jokes", "facts"]
+          }]} onSubmit={this.generateMedia} buttonText="Create my image!" />
         </div>
       </div>
     );
