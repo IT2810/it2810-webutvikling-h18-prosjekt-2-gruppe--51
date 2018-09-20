17c73775c6e301fe094199e197df9d8330eec530
Rikard Eriksen <rikarder@stud.ntnu.no>
2018-09-20 11:00:40 +0200

    current implementation to use new Text, Audio components

--- proj2/src/App.js
+++ proj2/src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import SelectorPanel from "./SelectorPanel.js"
+import AudioPanel from "./AudioPanel.js"
+import TextPanel from "./TextPanel.js"
 import axios from 'axios';
 
 class App extends Component {
 
   constructor(props) {
     super(props)
-    this.state = { text: { author: "Author placeholder", text: "Text placeholder" }, image: "<h1>Image placeholder</h1>" };
+    this.state = {
+      text: <TextPanel />,
+      image: "<h1>Image placeholder</h1>",
+      audio: <AudioPanel />
+    };
     this.generateMedia = this.generateMedia.bind(this);
   }
 
@@ -15,19 +21,18 @@ class App extends Component {
   // after this is done, the active tab should load and save its contents, but the others should wait until they activate
   // make sure to flush the content 
   generateMedia(results) {
-    const print = Object.values(results);
-    const prant = Object.keys(results)
-    alert("you have selected: " + prant + " : " + print);
-    axios.get(this.randomMedia({folder: "text", format: ".json"}, [results["text"]]))
+    axios.get(this.randomMedia({ folder: "text", format: ".json" }, [results["text"]]))
       .then(res => {
-        console.log(res);
-        this.setState({ text: res.data });
+        this.setState({ text: <TextPanel json={res.data} /> });
       });
 
     // todo: feed this URL to the tab components, and let them make an axios get when they are active
     let imageUrl = this.randomMedia({ folder: "images", format: ".svg" }, [results["image"]])
-    console.log(imageUrl)
     axios.get(imageUrl).then(res => { this.setState({ image: res.data }) });
+
+    let audioURL = this.randomMedia({ folder: "audio", format: ".mp3" }, [results["audio"]])
+    const audioPanel = <AudioPanel url={audioURL} />
+    this.setState({ audio: audioPanel })
   };
 
   randomMedia(type, categories) {
@@ -44,7 +49,12 @@ class App extends Component {
     return (
       <div className="App">
         <div className="GalleryImage" dangerouslySetInnerHTML={{ __html: image }} />
-        <div className="TextField"><h3>{this.state.text["author"]}</h3>{this.state.text["text"]}</div>
+        <div className="TextField">
+          {this.state.text}
+        </div>
+        <div className="AudioArea">
+          {this.state.audio}
+        </div>
         <div className="TabRow">
           <div className="t1">Tab1</div>
         </div>
@@ -58,6 +68,11 @@ class App extends Component {
             groupName: "text",
             legend: "Choose a text category",
             options: ["animal", "flowers", "summer"]
+          },
+          {
+            groupName: "audio",
+            legend: "Choose a sound category",
+            options: ["city", "nature", "sport"]
           }]} onSubmit={this.generateMedia} buttonText="Create my image!" />
         </div>
       </div>
