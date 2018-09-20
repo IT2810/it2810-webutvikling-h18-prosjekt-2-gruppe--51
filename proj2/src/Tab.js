153d42910d6491af2dd805a96b2e0fe4d002be65
jorgenhlehne <32197885+jorgenhlehne@users.noreply.github.com>
2018-09-20 16:24:22 +0200

    Added tab functionality and unfinished tab css
    How to use tabs is found in the Tabs.js folder. #5

---
+++ proj2/src/Tab.js
@@ -0,0 +1,23 @@
+import React, { Component} from 'react';
+
+class Tab extends Component {
+  onClick = () => {
+    this.props.onClick(this.props.label);
+  }
+
+  render() {
+    let className="tab-individual"
+    if (this.props.activeTab === this.props.label) {
+      className += " tab-current";
+    }
+
+    return (
+      <li className={className}
+      this.onClick={this.onClick} style={{cursor: "pointer"}}>
+        {this.props.label}
+      </li>
+    )
+  }
+}
+
+export default Tab;
